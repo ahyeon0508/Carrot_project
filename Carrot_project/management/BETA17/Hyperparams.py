9bba09f001f541cc05dcf98e7a1c3252209864ef
Jihyun <bjih1999@naver.com>
2020-08-13 14:37:15 +0900

    수정

--- Carrot_project/management/BETA17/Hyperparams.py
+++ Carrot_project/management/BETA17/Hyperparams.py
@@ -4,5 +4,5 @@ NODES = 24
 EPISODES = 10000
 MAX_STEPS = 200
 # PATH = 'Beta-17.pth'
-# PATH = 'management\BETA17\Beta-17.pth'
-PATH = 'Carrot_project\management\BETA17\Beta-17.pth'
+# PATH = 'management\BETA17\Beta-17.pth'    #manage.py 와 같은 위치
+PATH = 'Carrot_project\management\BETA17\Beta-17.pth'   #git 최상휘 위치
