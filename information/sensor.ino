aa1ff2a3c4b59397f93301920d759c442a1c70b0
ahyeon <ahyeon0508@naver.com>
2020-09-01 10:55:18 +0900

    carrot_information_1

---
+++ information/sensor.ino
@@ -0,0 +1,22 @@
+#include "DHT.h"     
+#include <ArduinoJson.h>
+#define DHTPIN 2     
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE); 
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float tempvalue = dht.readTemperature();  // 변수 t에 온도 값을 저장
+  float moisture = analogRead(A0);  //토양수분센서값 읽기
+  String jsondata = "";
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& object = jsonBuffer.createObject();
+  object["tempvalue"] = tempvalue;
+  object["moisture"] = moisture;
+  object.printTo(jsondata); //String으로 변환
+  Serial.println(jsondata);
+  delay(3600000);
+}
